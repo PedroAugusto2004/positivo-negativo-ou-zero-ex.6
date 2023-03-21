cc3a9c9394795008adcaa55531a6b0846f0f4c58
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2023-03-21 18:07:00 -0300

    Create num.py

---
+++ num.py
@@ -0,0 +1,10 @@
+number= float(input("Digite aqui seu número"))
+
+if number > 0:
+    print("O número é positivo.")
+
+elif number < 0:
+    print("O número é negativo")
+
+else:
+    print("O número é zero.")
